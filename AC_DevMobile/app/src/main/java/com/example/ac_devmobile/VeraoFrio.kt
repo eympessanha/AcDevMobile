d5e46edaa61db4dc9947f65026564b0d848d87e7
Esther <eympessanha@gmail.com>
2024-06-06 21:23:07 -0300

    lowcode pronto

---
+++ AC_DevMobile/app/src/main/java/com/example/ac_devmobile/VeraoFrio.kt
@@ -0,0 +1,20 @@
+package com.example.ac_devmobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class VeraoFrio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_verao_frio)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
